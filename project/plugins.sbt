82731eebc05d64d824cab2e11b7bda897bf6cbec
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-11-16 21:31:04 +0900

    Remove Old template files

--- project/plugins.sbt
+++
@@ -1,9 +0,0 @@
-/*
- * sbt-scalariform
- */
-addSbtPlugin("org.scalariform" % "sbt-scalariform" % "1.8.2")
-
-/*
- * sbt-scoverage
- */
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.1")
--- src/test/scala/com/github/nabezokodaikon/MainSpec.scala
+++
@@ -1,9 +0,0 @@
-package com.github.nabezokodaikon
-
-import org.scalatest.FunSuite                       
-
-class MainSpec extends FunSuite {                   
-  test("helloWorld") {                              
-    assert(Main.helloWorld("nabezokodaikokn") == "Hello nabezokodaikokn!")
-  }                                                 
-} 

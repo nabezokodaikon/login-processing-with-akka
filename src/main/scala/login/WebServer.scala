dbc08d9b5c9cdac4534b7e16bdcc7df96746bbd4
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-11-21 21:46:00 +0900

    Modify URI design

--- src/main/scala/login/WebServer.scala
+++ src/main/scala/login/WebServer.scala
@@ -25,6 +25,7 @@ import com.softwaremill.session.{
 }
 import com.softwaremill.session.SessionDirectives._
 import com.softwaremill.session.SessionOptions._
+import com.softwaremill.session.SessionResult._
 
 case class UserSession(userId: String)
 
@@ -58,7 +59,7 @@ class ServerApi(system: ActorSystem)
 
   // ブラウザ終了後もセッションを維持する場合。
   private def userSetSession(session: UserSession) = setSession(refreshable, usingCookies, session)
-  private val userRequiredSession = requiredSession(refreshable, usingCookies)
+  // private val userRequiredSession = requiredSession(refreshable, usingCookies)
   private val userInvalidateSession = invalidateSession(refreshable, usingCookies)
 
   // ブラウザ終了時にセッションを終了する場合。
@@ -83,25 +84,54 @@ class ServerApi(system: ActorSystem)
   }
 
   def routes: Route =
-    publicRoute ~
-      loginRoute ~
-      privateRoute
+    contentsRoute ~
+      loginRoute
 
-  private def publicRoute =
+  private def contentsRoute =
     pathSingleSlash {
       get {
-        val file = s"${publicDirectory}/index.html"
-        complete(createEntity(file))
+        session(refreshable, usingCookies) { sessionResult =>
+          sessionResult match {
+            case Decoded(session) =>
+              println(session)
+              val file = s"${privateDirectory}/index.html"
+              complete(createEntity(file))
+            case DecodedLegacy(session) =>
+              println(session)
+              val file = s"${privateDirectory}/index.html"
+              complete(createEntity(file))
+            case CreatedFromToken(session) =>
+              println(session)
+              val file = s"${privateDirectory}/index.html"
+              complete(createEntity(file))
+            case _ =>
+              val file = s"${publicDirectory}/index.html"
+              complete(createEntity(file))
+          }
+        }
       }
     } ~
       pathPrefix("contents") {
         path(Segments) { x: List[String] =>
           get {
-            val segments = x.mkString("/")
-            val path = s"${publicDirectory}/dir/${segments}"
-            path match {
-              case f if FileUtil.exists(f) => complete(createEntity(f))
-              case _ => complete(StatusCodes.NotFound)
+            session(refreshable, usingCookies) { sessionResult =>
+              sessionResult match {
+                case Decoded(session) =>
+                  println(session)
+                  val segments = x.mkString("/")
+                  val path = s"${privateDirectory}/dir/${segments}"
+                  path match {
+                    case f if FileUtil.exists(f) => complete(createEntity(f))
+                    case _ => complete(StatusCodes.NotFound)
+                  }
+                case _ =>
+                  val segments = x.mkString("/")
+                  val path = s"${publicDirectory}/dir/${segments}"
+                  path match {
+                    case f if FileUtil.exists(f) => complete(createEntity(f))
+                    case _ => complete(StatusCodes.NotFound)
+                  }
+              }
             }
           }
         }
@@ -121,7 +151,7 @@ class ServerApi(system: ActorSystem)
               println(s"userId: ${userId}, password: ${password}, isRememberMe: ${isRememberMe}")
               if (exampleUsers.contains(ExampleUser(userId, password))) {
                 userSetSession(UserSession(userId)) {
-                  redirect("member", StatusCodes.SeeOther)
+                  redirect("/", StatusCodes.SeeOther)
                 }
               } else {
                 redirect("login", StatusCodes.SeeOther)
@@ -133,30 +163,6 @@ class ServerApi(system: ActorSystem)
         get {
           userInvalidateSession {
             redirect("login", StatusCodes.SeeOther)
-          }
-        }
-      }
-
-  private def privateRoute =
-    path("member") {
-      get {
-        userRequiredSession { userSession =>
-          println(userSession)
-          val file = s"${privateDirectory}/index.html"
-          complete(createEntity(file))
-        }
-      }
-    } ~
-      path("member" / "contents" / Segments) { x: List[String] =>
-        get {
-          userRequiredSession { userSession =>
-            println(userSession)
-            val segments = x.mkString("/")
-            val path = s"${privateDirectory}/dir/${segments}"
-            path match {
-              case f if FileUtil.exists(f) => complete(createEntity(f))
-              case _ => complete(StatusCodes.NotFound)
-            }
           }
         }
       }

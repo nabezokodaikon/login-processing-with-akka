7718756317b4a1d79c2c5995f15a2bcd32030cae
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-11-25 22:16:33 +0900

    Add node create script

--- contents/public/header.js
+++ contents/public/header.js
@@ -1,1 +1,16 @@
 console.log("header");
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("DOM fully loaded and parsed");
+
+  var newDiv = document.createElement("div"); 
+  // いくつかの内容を与えます 
+  var newContent = document.createTextNode("Hi there and greetings!"); 
+  // テキストノードを新規作成した div に追加します
+  newDiv.appendChild(newContent);  
+
+  const bodyNode = document.getElementsByTagName('body').item(0);
+  const referenceNode = document.getElementsByTagName("h1").item(0);
+  bodyNode.insertBefore(newDiv, referenceNode);
+
+});
